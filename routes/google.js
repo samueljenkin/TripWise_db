525d0bd20a96fec810d01e383dfa7e2268eba17b
Sam26 <samueltjenkin@gmail.com>
2023-06-28 16:51:59 +0930

    update log out functionality

--- routes/google.js
+++ routes/google.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const fetch = require('node-fetch')
+
+require('dotenv').config()
+
 const { OPEN_CAGE_API_KEY, GOOGLE_PLACES_API_KEY, GOOGLE_PLACES_URL } = process.env
 
 router.post('/', (req, res) => {
